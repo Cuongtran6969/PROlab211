3a4a65006746fcf3fcc4f12fc8c644b5821db5a4
Cuongtran6969 <95119298+Cuongtran6969@users.noreply.github.com>
2023-06-04 17:31:52 +0700

    Add files via upload

---
+++ J1.S.P0007/graph/main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab211.graph;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HP
+ */
+public class main {
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Graph Graph = new Graph(6);
+        Graph.addEdge(1, 4);
+        Graph.addEdge(2, 4);
+        Graph.addEdge(2, 5);
+        Graph.addEdge(3, 5);
+        Graph.addEdge(4, 5);
+        int startPoint, endPoint;
+        System.out.println("Enter the start point: ");
+        startPoint = sc.nextInt();
+        System.out.println("Enter the end point: ");
+        endPoint = sc.nextInt();
+        System.out.println("This is"+(Graph.isEdge(startPoint, endPoint)?"":" not")+ " an edge");
+    }
+    
+    
+}
