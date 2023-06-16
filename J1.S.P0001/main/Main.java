2a17144c6fbad1df5f5a29030912f0d74a49cb1b
Cuongtran6969 <95119298+Cuongtran6969@users.noreply.github.com>
2023-06-16 11:36:57 +0700

    Update Main.java

--- J1.S.P0001/main/Main.java
+++ J1.S.P0001/main/Main.java
@@ -5,30 +5,31 @@
  */
 package main;
 import util.Validate;
-import logic.BubbleSort;
 import constant.IConstant;
+import logic.BubbleSort;
 /**
  *
  * @author HP
  */
 public class Main {
     public static void main(String[] args) {
+        BubbleSort bubbleSort = new BubbleSort();
         int arrLength;
         arrLength = Validate.getInt(
                 "Enter number of array: ",
                 "Input value out of range!",
                 "Invalid number!",
                 0, Integer.MAX_VALUE);
-        int[] arr = BubbleSort.randomArray(arrLength);
+        bubbleSort.generateRandomArray(arrLength);
         System.out.println("Unsorted array: ");
-        BubbleSort.displayArray(arr);
+        bubbleSort.displayArray();
         
         System.out.println("Ascending sort: ");
-        BubbleSort.sortArray(arr, IConstant.ASC);
-        BubbleSort.displayArray(arr);
+        bubbleSort.sortArray(IConstant.ASC);
+        bubbleSort.displayArray();
         
         System.out.println("Descending sort: ");
-        BubbleSort.sortArray(arr, IConstant.DESC);
-        BubbleSort.displayArray(arr);
+        bubbleSort.sortArray(IConstant.DESC);
+        bubbleSort.displayArray();
     }
 }
