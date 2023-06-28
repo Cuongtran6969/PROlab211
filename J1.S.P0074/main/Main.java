2e7c63d51144e02c750185e9e11b37d25d0d2c2d
Cuongtran6969 <95119298+Cuongtran6969@users.noreply.github.com>
2023-06-28 11:22:59 +0700

    Update Main.java

--- J1.S.P0074/main/Main.java
+++ J1.S.P0074/main/Main.java
@@ -26,20 +26,25 @@ public class Main {
                     "This option is not available!",
                     "Invalid input!",
                     1, 4);
-            int[][] matrix1 = logicMatrix.createMatrix(1);
-            int[][] matrix2 = logicMatrix.createMatrix(2);
+            int[][] matrix1, matrix2;
             switch (choice) {
                 case 1:
+                    matrix1 = logicMatrix.createMatrix(1);
+                    matrix2 = logicMatrix.createMatrix(2);
                     logicMatrix.additionMatrix(
                             matrix1, matrix2, Iconstant.ADDITION
                     );
                     break;
                 case 2:
+                    matrix1 = logicMatrix.createMatrix(1);
+                    matrix2 = logicMatrix.createMatrix(2);
                     logicMatrix.subtractionMatrix(
                             matrix1, matrix2, Iconstant.SUBTRACTION
                     );
                     break;
                 case 3:
+                    matrix1 = logicMatrix.createMatrix(1);
+                    matrix2 = logicMatrix.createMatrix(2);
                     logicMatrix.multiplicationMatrix(
                             matrix1, matrix2, Iconstant.MULTIPLICATION
                     );
