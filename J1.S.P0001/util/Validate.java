6b2250103dfe921c1b91221ddf4819a2f7fd734d
Cuongtran6969 <95119298+Cuongtran6969@users.noreply.github.com>
2023-06-30 07:42:01 +0700

    Update Validate.java

--- J1.S.P0001/util/Validate.java
+++ J1.S.P0001/util/Validate.java
@@ -17,30 +17,36 @@ public class Validate {
     /**
      * Get a valid integer input from the user within a specified range.
      *
-     * @param messInfo            notify to user input
-     * @param messErrorRange      notify when input value out of range
-     * @param messErrorInvalidate notify when input value wrong format
-     * @param min                 minimum limit
-     * @param max                 maximum limit
-     * @return the integers number validate
+     * @param messInfo            the message to be printed instructing the user to input
+     * @param messErrorRange      the message will be printed if the String parse
+     *                            value is out of range
+     * @param messErrorInvalidate the message will be printed if the String does
+     *                            not contain a portable integer
+     * @param min                 minimum limit value
+     * @param max                 maximum limit value
+     * @return the valid integer value scanned from the input
      */
     public static int getInt(
-            String messInfo,
-            String messErrorRange, 
-            String messErrorInvalidate,
-            int min, int max
-    ) {
+            String messInfo, String messErrorRange,
+            String messErrorInvalidate, int min, int max) {
         do {
             try {
                 System.out.print(messInfo);
-                int number = Integer.parseInt(SCANNER.nextLine());
-                if (number >= min && number <= max) {
-                    return number;
+                String input = SCANNER.nextLine();
+                if (input.isEmpty()) {
+                    return 0;
+                } else {
+                    int number = Integer.parseInt(input);
+                    if (number >= min && number <= max) {
+                        return number;
+                    }
+                    System.out.println(messErrorRange);
                 }
-                System.out.println(messErrorRange);
+
             } catch (NumberFormatException e) {
                 System.out.println(messErrorInvalidate);
             }
+
         } while (true);
-    
+    }
 }
